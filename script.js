4212145116f9d7b49cdafdbccb95c8dae75fc5c4
ArushiiGupta <arushi.bhavan@gmail.com>
2023-07-16 16:27:40 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+//const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '3254bdbc9fmsh694bc062897eaf5p154478jsna86ce061ac1e',
+		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+const getWeather = (city)=>{
+
+	cityname.innerHTML=city
+fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, options)
+	.then(response => response.json())
+	.then(response => {
+
+
+		//cloud_pct.innerHTML = response.cloud_pct
+		temp.innerHTML = response.temp
+		temp2.innerHTML = response.temp
+		feels_like.innerHTML = response.feels_like
+		humidity.innerHTML = response.humidity
+		humidity2.innerHTML = response.humidity
+		min_temp.innerHTML = response.min_temp
+		max_temp.innerHTML = response.max_temp
+		wind_speed.innerHTML = response.wind_speed
+		wind_speed2.innerHTML = response.wind_speed
+		wind_degrees.innerHTML = response.wind_degrees
+		sunrise.innerHTML = response.sunrise
+		sunset.innerHTML = response.sunset
+	})
+	.catch(err => console.error(err));
+}
+
+ submit.addEventListener("click" ,(e)=>{
+	e.preventDefault()
+	getWeather(city.value)
+})
+
+getWeather("Delhi")
